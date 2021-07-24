7d1a19833287eb454cdccd0678a0391e75ba943f
Kirill Markin <kmarkin@mbsicorp.com>
2021-07-24 19:09:24 +0300

    Rename TextureLoader to OpenGlTextureLoader

--- src/Container/DependencyContainer.h
+++ src/Container/DependencyContainer.h
@@ -5,7 +5,7 @@
 #include "Window/Window.h"
 #include "Application/AppParams.h"
 #include "Assets/AssetsManager.h"
-#include "Assets/TextureLoader.h"
+#include "Assets/OpenGlTextureLoader.h"
 #include "Render/Renderer.h"
 #include "Input/InputManager.h"
 
@@ -16,7 +16,7 @@ public:
     std::shared_ptr<InputManager> _inputManager;
     std::shared_ptr<AppParams> _appParams;
     std::shared_ptr<AssetsManager> _assetsManager;
-    std::shared_ptr<TextureLoader> _textureLoader;
+    std::shared_ptr<OpenGlTextureLoader> _textureLoader;
     std::shared_ptr<Renderer> _renderer;
 };
 
