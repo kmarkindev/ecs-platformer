26e433f32bed7d3dc06d0923cbf2b0f061cd9774
Kirill Markin <kmarkin@mbsicorp.com>
2021-08-18 14:55:59 +0300

    First version of FirstLevel Add ground, and it's collision. Add player with placeholder sprite and configured physics

--- src/Scene/SceneFactories/FirstLevel.cpp
+++ src/Scene/SceneFactories/FirstLevel.cpp
@@ -2,5 +2,74 @@
 
 std::shared_ptr<Scene> FirstLevel::LoadScene()
 {
-    return std::shared_ptr<Scene>();
+    auto cache = DependencyContainer::GetInstance()->_assetsCache;
+    _scene = std::make_shared<Scene>();
+
+    SetupCamera();
+
+    SetupGround(cache);
+
+    SetupPlayer(cache);
+
+    SetupSystems();
+
+    return _scene;
+}
+
+void FirstLevel::SetupPlayer(const std::shared_ptr<AssetsCache>& cache)
+{
+    auto player = _scene->CreateEntity();
+    player.AddComponent<TransformComponent>();
+    player.AddComponent<SpriteComponent>(&cache->_doorSprite);
+    player.AddComponent<PhysicsComponent>();
+}
+
+void FirstLevel::SetupSystems()
+{
+    _scene->AddSystem<SpriteRenderSystem>();
+    _scene->AddSystem<PhysicsSystem>();
+}
+
+void FirstLevel::SetupGround(const std::shared_ptr<AssetsCache>& cache)
+{
+    for(int x = -10; x <= 10; ++x)
+    {
+        auto groundGrassTile = _scene->CreateEntity();
+        groundGrassTile.AddComponent<TransformComponent>(glm::vec2(x, -2));
+        groundGrassTile.AddComponent<SpriteComponent>(&cache->_groundGrassSprite);
+    }
+
+    for(int y = -3; y > -7; --y)
+    {
+        for(int x = -10; x <= 10; ++x)
+        {
+            auto groundGrassTile = _scene->CreateEntity();
+            groundGrassTile.AddComponent<TransformComponent>(glm::vec2(x, y));
+            groundGrassTile.AddComponent<SpriteComponent>(&cache->_groundSprite);
+        }
+    }
+
+    auto groundCollision = _scene->CreateEntity();
+    groundCollision.AddComponent<TransformComponent>(glm::vec2(0,-2), glm::vec2(30, 0.95));
+    groundCollision.AddComponent<PhysicsComponent>(1.0f, 1.0f, 0.2f, Body::BodyType::Static);
+
+    auto leftWallCollision = _scene->CreateEntity();
+    leftWallCollision.AddComponent<TransformComponent>(glm::vec2(-10,5), glm::vec2(1, 10));
+    leftWallCollision.AddComponent<PhysicsComponent>(1.0f, 1.0f, 0.2f, Body::BodyType::Static);
+
+    auto rightWallCollision = _scene->CreateEntity();
+    rightWallCollision.AddComponent<TransformComponent>(glm::vec2(10,5), glm::vec2(1, 10));
+    rightWallCollision.AddComponent<PhysicsComponent>(1.0f, 1.0f, 0.2f, Body::BodyType::Static);
+}
+
+void FirstLevel::SetupCamera()
+{
+    auto camera = _scene->CreateEntity();
+    camera.AddComponent<TransformComponent>();
+    camera.AddComponent<CameraComponent>(10.0f);
+}
+
+void FirstLevel::UnloadScene()
+{
+    _scene.reset();
 }
--- src/Scene/SceneFactories/FirstLevel.h
+++ src/Scene/SceneFactories/FirstLevel.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include "Scene/SceneFactory.h"
+#include "Container/DependencyContainer.h"
+#include "Components/TransformComponent.h"
+#include "Components/SpriteComponent.h"
+#include "Components/CameraComponent.h"
+#include "Components/PhysicsComponent.h"
+#include "Systems/SpriteRenderSystem.h"
+#include "Systems/PhysicsSystem.h"
 
 class FirstLevel : public ISceneFactory
 {
@@ -9,4 +16,15 @@ public:
     std::shared_ptr<Scene> LoadScene() override;
     void UnloadScene() override;
 
+private:
+
+    std::shared_ptr<Scene> _scene;
+
+    void SetupCamera();
+
+    void SetupGround(const std::shared_ptr<AssetsCache>& cache);
+
+    void SetupSystems();
+
+    void SetupPlayer(const std::shared_ptr<AssetsCache>& cache);
 };
