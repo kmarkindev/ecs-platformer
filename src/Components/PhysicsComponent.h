d437ce77d71c28cf7c9e19a9cf1dd8a75f9913b8
Kirill Markin <kmarkin@mbsicorp.com>
2021-08-18 14:53:31 +0300

    Add default values to each component's properties

--- src/Components/PhysicsComponent.h
+++ src/Components/PhysicsComponent.h
@@ -4,11 +4,11 @@
 
 struct PhysicsComponent
 {
-    float mass;
-    float momentOfInertia;
-    float friction;
-    Body::BodyType bodyType;
-    glm::vec2 collisionBoxSize;
+    float mass = 1.0f;
+    float momentOfInertia = 0.2f;
+    float friction = 0.2f;
+    Body::BodyType bodyType = Body::BodyType::Dynamic;
+    glm::vec2 collisionBoxSize = {1.0f, 1.0f};
     float angularVelocity = 0.0f;
     glm::vec2 linearVelocity = {0.0f, 0.0f};
 };
